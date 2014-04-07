29ded1f5a0481390d258643360dc62c441da87a6
khagendra <khagendra@khagendra-VirtualBox>
2014-04-07 19:48:59 +0200

    Start

---
+++ node.cpp
@@ -0,0 +1,18 @@
+/* 
+ * File:   node.cpp
+ * Author: khagendra
+ * 
+ * Created on 07 April 2014, 7:48 PM
+ */
+
+#include "node.h"
+
+node::node() {
+}
+
+node::node(const node& orig) {
+}
+
+node::~node() {
+}
+
---
+++ node.h
@@ -0,0 +1,21 @@
+/* 
+ * File:   node.h
+ * Author: khagendra
+ *
+ * Created on 07 April 2014, 7:48 PM
+ */
+
+#ifndef NODE_H
+#define	NODE_H
+
+class node {
+public:
+    node();
+    node(const node& orig);
+    virtual ~node();
+private:
+
+};
+
+#endif	/* NODE_H */
+
